9885af3935b8c5dc6075a2ea031d6abd487cbb72
Ellie Mears <eleanor.mears@gmail.com>
2022-09-26 18:00:50 +0200

    création de header.scss, header.js

---
+++ assets/script/header.js
@@ -0,0 +1,10 @@
+// récupération des éléments
+const menuArrow = document.getElementById("menu-arrow");
+console.log(menuArrow);
+const submenu = document.getElementById("submenu");
+
+
+// menuArrow.addEventListener('click', () => {
+//     submenu.classList.toggle("hidden");
+// })
+
