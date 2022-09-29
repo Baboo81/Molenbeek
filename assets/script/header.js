4de19d70587bf839fd6689b434b1c57ebdffac6e
Ellie Mears <eleanor.mears@gmail.com>
2022-09-29 08:54:26 +0200

    ajout de .gitignore

--- assets/script/header.js
+++ assets/script/header.js
@@ -1,10 +1,15 @@
-// récupération des éléments
-// const menuArrow = document.getElementById("menu-arrow");
-// console.log(menuArrow);
-// const submenu = document.getElementById("submenu");
+let submenu = document.querySelector('.submenu');
+let dropdown = document.querySelector("#first-child");
+let dropdownArrow  = document.querySelector("#dropdown-arrow");
+let subItems = document.querySelectorAll(".submenu li a");
 
 
-// menuArrow.addEventListener('click', () => {
-//     submenu.classList.toggle("hidden");
-// })
+dropdown.addEventListener("click", () => {
+    submenu.classList.toggle("hidden");
+    dropdownArrow.classList.toggle("rotate");
+    dropdownArrow.style.transition = "all ease-in 400ms";
+    dropdown.classList.toggle("active");
+    dropdown.style.transition = "all 400ms";
+});
 
+console.log(submenu, dropdown, dropdownArrow, subItems);
