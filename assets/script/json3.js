45f76ed921c022502ccb95a62aebd2256b5f1a84
Christel Rodriguez Perez <magicbaboo@gmail.com>
2022-10-07 11:05:11 +0200

    Modif photos

--- assets/script/json3.js
+++ assets/script/json3.js
@@ -2,7 +2,7 @@ const DATA = [
     {
         lat: 50.86048, 
         lng: 4.31557,
-        image: '/assets/img/itinéraire3/photo2.JPG',
+        image: '/assets/img/itinéraire3/photo2.JPG',
         lieu: 'Chateau du Karreveld',
         distance: '0',
         denivele: '0',
@@ -12,7 +12,7 @@ const DATA = [
         lat: 50.85968,
         lng: 4.34562,
         lieu: 'Sculpture de l\'agent',
-        image: '/assets/img/itinéraire3/flic.JPG',
+        image: '/assets/img/itinéraire3/flic.JPG',
         distance: null,
         denivele: null,
         difficulte: 'facile'
@@ -21,7 +21,7 @@ const DATA = [
         lat: 50.857623,
         lng: 4.325175,
         lieu: 'Gare aux fantômes',
-        image: '/assets/img/itinéraire3/photo12.JPG',
+        image: '/assets/img/itinéraire3/photo12.JPG',
         distance: null,
         denivele: null,
         difficulte: 'facile'
@@ -30,7 +30,7 @@ const DATA = [
         lat: 50.85802,
         lng: 4.33831,
         lieu: 'Maison culturelle de Molenbeek',
-        image: '/assets/img/itinéraire3/asbl.JPG',
+        image: '/assets/img/itinéraire3/asbl.JPG',
         distance: null,
         denivele: null,
         difficulte: 'facile'
@@ -39,7 +39,7 @@ const DATA = [
         lat: 50.854320,
         lng: 4.316353,
         lieu: 'L\' église St Jean-Baptiste',
-        image: '/assets/img/itinéraire3/saint-baptiste.JPG',
+        image: '/assets/img/itinéraire3/saint-baptiste.JPG',
         distance: null,
         denivele: null,
         difficulte: 'facile'
@@ -48,7 +48,7 @@ const DATA = [
         lat: 50.851194,
         lng: 4.313364,
         lieu: 'Maison communale',
-        image: '/assets/img/itinéraire3/placeCommunale.JPG',
+        image: '/assets/img/itinéraire3/placeCommunale.JPG',
         distance: null,
         denivele: null,
         difficulte: 'facile'
@@ -57,7 +57,7 @@ const DATA = [
         lat: 50.851597,
         lng: 4.300140,
         lieu: 'La Fonderie',
-        image: '/assets/img/itinéraire3/fonderie.JPG',
+        image: '/assets/img/itinéraire3/fonderie.JPG',
         distance: null,
         denivele: null,
         difficulte: 'facile'
