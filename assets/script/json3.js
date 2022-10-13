a41f934f510966a5c0e0f0cf13cdea52071cfeda
Person M <michael.person@cognitic.be>
2022-10-13 17:02:42 +0200

    Ajout des fichiers perdu suite au merge anarchique!

---
+++ assets/script/json3.js
@@ -0,0 +1,65 @@
+const DATA = [
+    {
+        lat: 50.86048, 
+        lng: 4.31557,
+        image: '/assets/img/itinéraire3/photo2.JPG',
+        lieu: 'Chateau du Karreveld',
+        distance: '0',
+        denivele: '0',
+        difficulte: 'facile'
+    },
+    {
+        lat: 50.85968,
+        lng: 4.34562,
+        lieu: 'Sculpture de l\'agent',
+        image: '/assets/img/itinéraire3/flic.JPG',
+        distance: null,
+        denivele: null,
+        difficulte: 'facile'
+    },
+    {
+        lat: 50.857623,
+        lng: 4.325175,
+        lieu: 'Gare aux fantômes',
+        image: '/assets/img/itinéraire3/photo12.JPG',
+        distance: null,
+        denivele: null,
+        difficulte: 'facile'
+    },
+    {
+        lat: 50.85802,
+        lng: 4.33831,
+        lieu: 'Maison culturelle de Molenbeek',
+        image: '/assets/img/itinéraire3/asbl.JPG',
+        distance: null,
+        denivele: null,
+        difficulte: 'facile'
+    },
+    {
+        lat: 50.854320,
+        lng: 4.316353,
+        lieu: 'L\' église St Jean-Baptiste',
+        image: '/assets/img/itinéraire3/saint-baptiste.JPG',
+        distance: null,
+        denivele: null,
+        difficulte: 'facile'
+    },
+    {
+        lat: 50.851194,
+        lng: 4.313364,
+        lieu: 'Maison communale',
+        image: '/assets/img/itinéraire3/placeCommunale.JPG',
+        distance: null,
+        denivele: null,
+        difficulte: 'facile'
+    },
+    {
+        lat: 50.851597,
+        lng: 4.300140,
+        lieu: 'La Fonderie',
+        image: '/assets/img/itinéraire3/fonderie.JPG',
+        distance: null,
+        denivele: null,
+        difficulte: 'facile'
+    }
+]
