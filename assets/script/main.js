5435d792fb52ac49e4ce0c47244eb0e0b8209f08
Ellie Mears <eleanor.mears@gmail.com>
2022-10-13 18:12:58 +0200

    modification de main.js

--- assets/script/main.js
+++ assets/script/main.js
@@ -1,4 +1,4 @@
-// swap intro image for bigger version when screen width above 1025px using JS media queries
+// Remplacez l'image "Molenbeek-scene-mobile-outline.svg" par "Molenbeek-scene-lg.svg" lorsque la largeur de l'écran dépasse 1025px en utilisant les requêtes multimédias JS. Même chose pour le logo.
 // récupérer élément
 const cityscape = document.getElementById("cityscape");
 const logo = document.querySelector("#intro-title img");
@@ -6,7 +6,7 @@ console.log(logo);
 
 function changePic(x) {
 
-    if (x.matches) { // Si media query correspond, changez le src de img#cityscape.
+    if (x.matches) { // Si le media query correspond, changez le src de img#cityscape.
       cityscape.src = "/assets/img/illustrations/Molenbeek-scene-lg.svg";
       logo.src = "/assets/img/logos/logo-uni-3.svg"
     } else {
@@ -17,3 +17,9 @@ function changePic(x) {
   
   let x = window.matchMedia("(min-width: 1025px)");
   changePic(x); // Call listener function at run time
+
+// recharger la page lorsque l'écran est redimensionné
+  window.addEventListener('resize', function () { 
+    "use strict";
+    window.location.reload(); 
+});
