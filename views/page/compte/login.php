dc90c35da4e87542d9ce7ab5ffcc7907ed938eed
Ellie Mears <eleanor.mears@gmail.com>
2022-10-20 10:51:38 +0200

    rajout de php dans login.php

--- views/page/compte/login.php
+++ views/page/compte/login.php
@@ -4,6 +4,14 @@
             <form class="form" method="post" action="/assets/script/account.php">
                 <h2>Login</h2>
                 <div class="form__fields">
+                <div class="input-group">
+                        <label for="prenom" class="input-group__label">Prénom :</label>
+                        <input type="email" name="prenom" id="prenom" class="input-group__input" placeholder="Entrez votre prénom" required>
+                    </div>
+                    <div class="input-group">
+                        <label for="nom" class="input-group__label">Nom de famille :</label>
+                        <input type="email" name="nom" id="nom" class="input-group__input" placeholder="Entrez votre nom de famille" required>
+                    </div>
                     <div class="input-group">
                         <label for="mail" class="input-group__label">Adresse mail :</label>
                         <input type="email" name="mail" id="mail" class="input-group__input" placeholder="Entrez votre adresse e-mail" required>
@@ -32,4 +40,74 @@
             </form>
 
         </div>
+        
     </main>
+    <?php
+    try {//Objet PDO permet la connection à la BD:
+        $mysqlClient = new PDO(
+            sprintf('mysql:host=%s;dbname=%s;port=%s', 
+            "localhost",
+            "membres",
+            3306),
+            "root",
+            "root"
+        );
+        $mysqlClient->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        
+    } catch(Exception $exception) {
+        die('La connection a échoué : '.$exception->getMessage());
+    }
+?>
+    
+    <?php 
+        /**
+         * Vérification de l'utilisateur
+         */
+        $isFound = false; //connu
+        $connectedUser;
+        $loginAttempt = 0;
+
+        if(isset($_POST['user'],$_POST['mail'],$_POST['password'])){
+            
+            $loginAttempt++;
+
+            $user_user = $_POST['user'];
+            $user_mail = $_POST['mail']; 
+            $pass_word = $_POST['password'];
+
+            $sqlQuery = "
+                SELECT *
+                FROM membres
+                WHERE username = :param_username AND passwd = :param_password 
+            ";
+
+            $statement = $mysqlClient->prepare($sqlQuery); 
+            $statement->execute(array("param_username" => $user_name, "param_password" => $pass_word));
+            $result = $statement->fetchAll();  
+         
+            if($result){
+                $isFound = true;
+                $connectedUser = $result[0]['username'];
+                $email = $result[0]['email'];
+            }
+
+        }
+
+        if($isFound){
+
+            echo "Bienvenue sur page d'accueil ! $connectedUser ($email)";
+
+        }else{
+
+
+            include_once('assets/views/login.html');
+
+
+            if(!$isFound && $loginAttempt >=1 ){
+                echo "Username and password incorrect! ";
+                $loginAttempt = 0;
+            }
+            
+        }
+
+    ?>
